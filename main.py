5fdc1fabb48bbe46427bce868e9886b56d0acefc
zhangtianye <tianye@cpu-os.ac.cn>
2018-05-27 23:05:11 +0800

    创建新工程

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/bin/python
+#-*-coding:UTF-8-*-
+
+from __future__ import unicode_literals
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index!'
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(host="0.0.0.0")
---
+++ readme.txt
@@ -0,0 +1,1 @@
+基于Flask的Blog
